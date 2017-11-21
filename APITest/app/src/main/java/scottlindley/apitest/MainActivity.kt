3ee523be80cb8b15788a8a1ece663282020b4b68
Scott Lindley <scottdlindley@gmail.com>
2017-11-20 20:01:00 -1000

    added support for other methods

--- APITest/app/src/main/java/scottlindley/apitest/MainActivity.kt
+++ APITest/app/src/main/java/scottlindley/apitest/MainActivity.kt
@@ -3,11 +3,16 @@ package scottlindley.apitest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpGet
-import scottlindley.apitest.R.id.responseTextView
+import kotlinx.android.synthetic.main.activity_main.*
 import scottlindley.apitest.R.layout.activity_main
+import android.widget.ArrayAdapter
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.result.Result
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +20,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
 
-        FuelManager.instance.basePath = "https://snapsmedia.io/api/"
-
-        val endpoint = "https://snapsmedia.io/api/discover/"
-
-        val handleJson = fun (json: String?) {
-            Log.d("handleJson", json)
+        val handleJson = fun (json: Any?) {
             if (json == null) return
-            findViewById<TextView>(responseTextView).setText(json)
+            try {
+                Log.d("handleJson", json.toString())
+                responseTextView.text = json.toString()
+            } catch (e : Exception) { Log.e("handleJson", e.toString()) }
         }
 
-        hitApi(endpoint, handleJson)
+        val items = arrayOf("GET", "POST", "PUT", "DELETE")
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+
+        submitButton.setOnClickListener({
+            hitApi(urlInput.text.toString(), spinner.selectedItem.toString(), handleJson)
+        })
     }
 
-    fun hitApi(endpoint: String, cb: (String?) -> Unit) {
-        endpoint.httpGet().responseString { request, response, result ->
+    fun hitApi(endpoint: String, method: String, cb: (Any?) -> Unit, body: String? = null) {
+        val handleResp = fun (request: Request, response: Response, result: Result<Any, FuelError>) {
             Log.d("Request:", request.toString())
             Log.d("Response:", response.toString())
             val (data, error) = result
@@ -37,6 +45,36 @@ class MainActivity : AppCompatActivity() {
                 cb(data)
             } else {
                 cb(error.toString())
+            }
+        }
+
+        when (method) {
+            "GET" -> {
+                endpoint.httpGet().responseString { request, response, result ->
+                    handleResp(request, response, result)
+                }
+            }
+            "POST" -> {
+                if (body != null) {
+                    Fuel.post(endpoint).body(body).response { request, response, result ->
+                        handleResp(request, response, result)
+                    }
+                }
+                else {
+                    Fuel.post(endpoint).response { request, response, result ->
+                        handleResp(request, response, result)
+                    }
+                }
+            }
+            "PUT" -> {
+                Fuel.put(endpoint).response { request, response, result ->
+                    handleResp(request, response, result)
+                }
+            }
+            "DELETE" -> {
+                Fuel.delete(endpoint).response { request, response, result ->
+                    handleResp(request, response, result)
+                }
             }
         }
     }
